4f53e77838de4cff66deef994b36e8802311cfda
Curtis Swartzentruber <curt@swartzentruber.net>
2013-03-18 09:51:24 -0400

    remove api config stuff

--- MVC4TestForGitHub/MVC4TestForGitHub/Global.asax.cs
+++ MVC4TestForGitHub/MVC4TestForGitHub/Global.asax.cs
@@ -18,7 +18,6 @@ namespace MVC4TestForGitHub
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
